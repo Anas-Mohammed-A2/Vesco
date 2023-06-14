c3cd75db74b2348d860d8306ed929c343522bf71
FunDev2 <fundev233@gmail.com>
2023-06-14 09:16:32 +0200

    add vesco

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+// import react roter
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Style
+import "./assets/Css/loding.css";
+// Cmponts
+import Home from "./assets/Pages/Home";
+import About from "./assets/Pages/About.jsx";
+import Navv from "./assets/Pages/Navv";
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {loading ? (
+        <div className="preloaderr">
+        <img src="../imgs/logos/viabox (2).png" alt="logo" className="imglogo"/>
+        <span class="loader"></span>
+        </div>
+      ) : (
+        <>
+      
+        <Navv/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+          </Routes>
+        </>
+      )}
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
